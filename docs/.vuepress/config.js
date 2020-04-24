1e00aacc595ca6ca3ed4b790479ae5264a777ad4
lizhensheng <1841084366@qq.com>
2020-04-24 09:00:16 +0800

    modify document

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const { fs, path } = require('@vuepress/shared-utils')
 
 module.exports = ctx => ({
-  base: '/vuepress/',
+  base: '/vue-data-view/vuepress/',
   dest: './vuepress',
   locales: {
     '/zh/': {
